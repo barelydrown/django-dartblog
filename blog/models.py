0f67d0f21dd52c07234d8c697793262ccfdc8f36
barelydrown <jooomanfirst@yandex.ru>
2021-10-20 15:46:52 +0300

    models, static, settings

---
+++ blog/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    views = models.IntegerField(default=0)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.PROTECT,
+        related_name='posts'
+    )
+    tags = models.ManyToManyField('Tag', blank=True, related_name='tags')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
