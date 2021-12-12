61cf52a3bea2b6acccf819782eff0b356ccdc38b
barelydrown <jooomanfirst@yandex.ru>
2021-12-12 15:38:07 +0300

    edit settings.py

--- dartblog/settings.py
+++ dartblog/settings.py
@@ -22,11 +22,12 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps:
     'blog.apps.BlogConfig',
-    'api.apps.ApiConfig',
 
     # Installed apps:
     'debug_toolbar',
     'ckeditor',
+    'rest_framework',
+    'djoser',
 
     # Default apps:
     'django.contrib.admin',
@@ -142,6 +143,28 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': False,
+    'SERIALIZERS': {},
+    'TOKEN_MODEL': 'rest_framework.authtoken.models.Token',
 }
 
 CKEDITOR_CONFIGS = {
