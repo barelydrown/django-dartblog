377044ce286204a0cb911e71db6bad03d05c28c5
barelydrown <jooomanfirst@yandex.ru>
2021-12-12 16:35:07 +0300

    add views api/views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from blog.models import Category, Tag, Post
+from .serializers import CategorySerializer
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
