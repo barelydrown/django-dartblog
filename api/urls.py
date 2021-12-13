8e87f726c1d33a3a293d9fec5003281dd6c1ef69
barelydrown <jooomanfirst@yandex.ru>
2021-12-13 23:27:44 +0300

    minor syntax fixes 1

--- api/urls.py
+++ api/urls.py
@@ -12,26 +12,26 @@ from .views import (
 )
 
 urlpatterns = [
-    # Auth
+    # auth
     path('auth/', include('djoser.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refersh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # Category Model
+    # category model
     path('category/all/', CategoryListView.as_view()),
     path('category/<int:pk>/', CategoryRetrieveView.as_view()),
     path('category/new/', CategoryCreateView.as_view()),
     path('category/update/<int:pk>/', CategoryUpdateView.as_view()),
     path('category/delete/<int:pk>/', CategoryDestroyView.as_view()),
 
-    # Tag Model
+    # tag model
     path('tag/all/', TagListView.as_view()),
     path('tag/<int:pk>/', TagRetrieveView.as_view()),
     path('tag/new/', TagCreateView.as_view()),
     path('tag/update/<int:pk>/', TagUpdateView.as_view()),
     path('tag/delete/<int:pk>/', TagDestroyView.as_view()),
 
-    # Post Model
+    # post model
     path('post/all/', PostListView.as_view()),
     path('post/<int:pk>/', PostRetrieveView.as_view()),
     path('post/new/', PostCreateView.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,6 +1,4 @@
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 
 from blog.models import Category, Tag, Post
